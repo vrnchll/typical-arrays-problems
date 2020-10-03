91825190bdca82e4bff666396a3390eb6d796d0f
vrnchll <veronichka127@mail.ru>
2020-10-03 19:11:52 +0300

    feat task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	 if (array && array.length !== 0) 
+	 {
+    let result = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < result) {
+            result = array[i];
+        }
+    }
+    return result;
+	 }
+	 else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+   if (array && array.length !== 0) 
+	 {
+    let result = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > result) {
+            result = array[i];
+        }
+    }
+    return result;
+	 }
+	 else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array && array.length !== 0) 
+	 {
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+    return result / array.length;
+	 }
+	 else return 0;
 }
